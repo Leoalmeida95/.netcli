a5ea36113bc03a86895408479bf34e3353a62ed2
Leonardo Almeida da Silva <leonardo.silva@intranet.cesgranrio.org.br>
2020-01-09 14:28:51 -0300

    index

--- NgTemplate/src/app/app.component.ts
+++ NgTemplate/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NgTemplate';
+  title = 'Tudo pronto!';
+  isCollapsed: boolean = true;
 }
--- NgTemplate/src/app/app.module.ts
+++ NgTemplate/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule.forRoot(), //utiliza o forRoot para que o componente entenda que se trata do modulo raiz
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
