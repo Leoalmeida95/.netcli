a4ca361960481b8c396db9592bebf4b8073fc937
Leonardo Almeida da Silva <leonardo.silva@intranet.cesgranrio.org.br>
2020-01-09 17:24:29 -0300

    routes

--- NgTemplate/src/app/app.module.ts
+++ NgTemplate/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {rootRouterConfig} from './app.routes';
 
 //boostrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuLoginComponent } from './shared/menu-login/menu-login.component';
+import { ListaEventosComponent } from './eventos/lista-eventos/lista-eventos.component';
 
 @NgModule({
   declarations: [
@@ -24,14 +27,16 @@ import { MenuLoginComponent } from './shared/menu-login/menu-login.component';
     FooterComponent,
     MainPrincipalComponent,
     HomeComponent,
-    MenuLoginComponent
+    MenuLoginComponent,
+    ListaEventosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(), //utiliza o forRoot para que o componente entenda que se trata do modulo raiz
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    RouterModule.forRoot(rootRouterConfig, {useHash: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
