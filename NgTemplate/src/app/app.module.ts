e7dcd2e1859bfb5974231d023df7a6f19ed65235
Leonardo Almeida da Silva <leonardo.silva@intranet.cesgranrio.org.br>
2020-01-09 15:11:06 -0300

    footer

--- NgTemplate/src/app/app.module.ts
+++ NgTemplate/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuSuperiorComponent,
+    FooterComponent,
+    MainPrincipalComponent
   ],
   imports: [
     BrowserModule,
