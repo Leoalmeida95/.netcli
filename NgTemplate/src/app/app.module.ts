b08afe964ae2eb889af2a15bc2611811d12de482
Leonardo Almeida da Silva <leonardo.silva@intranet.cesgranrio.org.br>
2020-01-09 15:22:21 -0300

    home/placeholder

--- NgTemplate/src/app/app.module.ts
+++ NgTemplate/src/app/app.module.ts
@@ -2,26 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//boostrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//shared
 import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
+
+//componentees
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuSuperiorComponent,
     FooterComponent,
-    MainPrincipalComponent
+    MainPrincipalComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     CollapseModule.forRoot(), //utiliza o forRoot para que o componente entenda que se trata do modulo raiz
-    BrowserAnimationsModule
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
